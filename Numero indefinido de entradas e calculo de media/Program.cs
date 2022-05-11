bb62b864d9c06fdbb677d5e92b0ee0f6983a645b
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-10 22:03:10 -0300

    Média valores numeros

---
+++ Numero indefinido de entradas e calculo de media/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Numero_indefinido_de_entradas_e_calculo_de_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double entrada,media,resultado=0;
+            int contador=0;
+
+            do
+            {
+
+                Console.WriteLine("insira um  valor de entrada: ");
+                entrada=double.Parse(Console.ReadLine());
+                if (entrada > 0)
+                {
+                    resultado = resultado + entrada;
+                    contador++;
+                }
+
+            }
+            while (entrada > 0);
+
+            media = resultado / contador;
+
+            Console.WriteLine("a media foi: " + media);
+        }
+    }
+}
